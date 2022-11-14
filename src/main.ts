44265913c6bae7304aad93526300c9e5a54c57e3
Abdul Rehman Shahid <ars7.dev@gmail.com>
2022-11-14 19:04:03 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-localStorage.setItem('backend', 'http://127.0.0.1:8000/')
+localStorage.setItem('backend', 'http://ars-hd loca.lt/')
 localStorage.setItem('default', 'true')
 
 const app = createApp(App)
